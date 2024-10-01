134081bdfb2d1479289351b3e065add7efa8cab6
JuanDev <juanmoran@unicauca.edu.co>
2024-10-01 15:44:14 -0500

    Completando plugin y agregando dependencias

--- PluginSimpleJavaMail/src/main/java/plugin/simple/java/mail/SimpleJava.java
+++ PluginSimpleJavaMail/src/main/java/plugin/simple/java/mail/SimpleJava.java
@@ -1,30 +1,34 @@
 package plugin.simple.java.mail;
 
+import co.edu.unicauca.cuychair.common.notificationPlugin.IEmailPlugin;
+import domain.Conference;
+import domain.Paper;
+import domain.User;
 import org.simplejavamail.api.email.Email;
 import org.simplejavamail.api.mailer.Mailer;
 import org.simplejavamail.api.mailer.config.TransportStrategy;
 import org.simplejavamail.email.EmailBuilder;
 import org.simplejavamail.mailer.MailerBuilder;
 
-public class SimpleJava {
+public class SimpleJava implements IEmailPlugin {
+    @Override
+    public void notifySendJobs(Paper paper, Conference conference, User author) {
+        Mailer ma= MailerBuilder.withSMTPServer("smtp.gmail.com", 587, "cuychair@gmail.com", "cies amcp hfot rxhh")
+                .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                .buildMailer();
 
-    public void notifySendJob() implements IEmailPlugin{
+        Email email= EmailBuilder.startingBlank()
+                .from("Notificaciones cuyChair","cuychair@gmail.com")
+                .to("Hola",author.getUserEmail())
+                .withSubject("Estado de los productos")
+                .withPlainText("Estimado autor\n\n\n" +
+                        "Notificamos que se recibió de forma exitosa su trabajo para la Conferencia: "+conference.getTitle()+"\n\n"
+                        +"Autor: "+author.getUserName()+"\n"
+                        +"Trabajo: "+paper.getTitle()+"\n\n\n\n"
+                        +"Gracias por todo su esfuerzo :)")
+                .buildEmail();
 
-        @Override
-        public void notifySendJob(Paper job, Conference confe,User author){
-            Mailer ma= MailerBuilder.withSMTPServer("smtp.gmail.com", 587, "cuychair@gmail.com", "cies amcp hfot rxhh")
-                    .withTransportStrategy(TransportStrategy.SMTP_TLS)
-                    .buildMailer();
-
-            Email email= EmailBuilder.startingBlank()
-                    .from("Notificaciones cuyChair","cuychair@gmail.com")
-                    .to("Hola",author.getEmail())
-                    .withSubject("Estado de los productos")
-                    .withPlainText("Los productos se agregaron correctamente")
-                    .buildEmail();
-
-            ma.sendMail(email);
-        }
+        ma.sendMail(email);
     }
 
 }
--- cuyChair-common/src/main/java/co/edu/unicauca/cuychair/common/notificationPlugin/IEmailPlugin.java
+++ cuyChair-common/src/main/java/co/edu/unicauca/cuychair/common/notificationPlugin/IEmailPlugin.java
@@ -2,8 +2,9 @@ package co.edu.unicauca.cuychair.common.notificationPlugin;
 
 import domain.Conference;
 import domain.Paper;
+import domain.User;
 
 public interface IEmailPlugin {
 
-    void notifySendJobs(Paper job, Conference confe);
+    void notifySendJobs(Paper job, Conference confe, User author);
 }
