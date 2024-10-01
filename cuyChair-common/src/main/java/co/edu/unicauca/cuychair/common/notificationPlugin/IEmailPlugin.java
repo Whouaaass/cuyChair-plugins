a995e4243cde8e0743c8d3940eb3e0ae89395795
JuanDev <juanmoran@unicauca.edu.co>
2024-10-01 15:26:15 -0500

    Agregando dependencias y el contrato del plugin

--- cuyChair-common/src/main/java/co/edu/unicauca/cuychair/common/notificationPlugin/IEmailPlugin.java
+++ cuyChair-common/src/main/java/co/edu/unicauca/cuychair/common/notificationPlugin/IEmailPlugin.java
@@ -1,6 +1,9 @@
 package co.edu.unicauca.cuychair.common.notificationPlugin;
 
+import domain.Conference;
+import domain.Paper;
+
 public interface IEmailPlugin {
 
-    void notifySendJobs(Paper job,Conference confe);
+    void notifySendJobs(Paper job, Conference confe);
 }
